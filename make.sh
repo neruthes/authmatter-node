98e348d8d9020d3a900cffead342fa352d2fad9d
Neruthes <i@neruthes.xyz>
2025-04-15 23:16:44 +0000

    Regular update '2025-04-15T23:16:44+00:00'

--- make.sh
+++ make.sh
@@ -5,5 +5,10 @@ case $1 in
     dep | node_modules | node_modules/ )
         yarn
         ;;
+    examples/ )
+        find examples -name '*.toml' | while read -r line; do
+            tomlq . "$line" > "${line/toml/json}"
+        done
+        ;;
 esac
 
--- src/am-server.js
+++ src/am-server.js
@@ -12,7 +12,41 @@
 
 import fs from 'fs';
 import express from 'express';
+let express_gateway = express();
+
 import { Provider } from 'oidc-provider';
+
+
+
+
+
+// Bring object definitions to top
+const TmpClientIdMapRedirUriMap = {};
+
+
+
+
+
+
+
+
+let AuthMatterStartupConfig = {
+    startup_config_path: process.argv[2] || process.env.AM_CONFIG_PATH,
+    startup_config_dict_in_memory: {},
+};
+const AuthMatterRuntimeConfigManager = {
+    load_startup_config: function () {
+        // TODO: Dynamic config reloading feature?
+        AuthMatterStartupConfig.startup_config_dict_in_memory = JSON.parse(fs.readFileSync(AuthMatterStartupConfig.startup_config_path));
+    }
+};
+AuthMatterRuntimeConfigManager.load_startup_config();
+
+function get_config() {
+    return AuthMatterStartupConfig.startup_config_dict_in_memory;
+}
+
+
 
 
 const fancy_stringify_json = function (obj) {
@@ -35,7 +69,7 @@ const DevMockDatabase = {
         1: {
             uid: 1,
             username_short: 'root', // Really stored in database
-            username: 'root@localhost', // Computed from `username_short` and `org_domain`
+            username: 'root@' + get_config().org_domain, // Computed from `username_short` and `org_domain`
             display_name: 'AuthMatterRootUser',
             user_roles: ['staff', 'admin'],
             is_domestic: true,
@@ -78,58 +112,212 @@ const SystemReservedRuntimeConstants = {
 
 
 
-let AuthMatterStartupConfig = {
-    startup_config_path: process.argv[2] || process.env.AM_CONFIG_PATH,
-    startup_config_dict_in_memory: {},
-};
-
-
-
-const AuthMatterRuntimeConfigManager = {
-    get_startup_config: function () {
-        AuthMatterStartupConfig.startup_config_dict_in_memory = JSON.parse(fs.readFileSync(AuthMatterStartupConfig.startup_config_path));
-        return AuthMatterStartupConfig.startup_config_dict_in_memory;
-    },
-    save_startup_config: function () {
-        // Hope we never need it!
-        fs.writeFileSync(AuthMatterStartupConfig.startup_config_path, JSON.stringify(AuthMatterStartupConfig.startup_config_dict_in_memory, '\t', 4));
-    },
-};
 
 
 
 
 
 
-// Empty config?
-// const oidc = new Provider('AuthMatterRuntimeConfigManager.get_startup_config().site_hostname', {});
 
-const clients = []; // empty! you can allow dynamic or override `findById`
+
+// ==================================================================================
+// Adapter needs Provider.ctx. Can I move it to another file and import?
+// ==================================================================================
+
+import QuickLRU from 'quick-lru';
+import epochTime from 'oidc-provider/lib/helpers/epoch_time.js';
+
+let storage = new QuickLRU({ maxSize: 1000 });
+
+function grantKeyFor(id) {
+    return `grant:${id}`;
+}
+
+function sessionUidKeyFor(id) {
+    return `sessionUid:${id}`;
+}
+
+function userCodeKeyFor(userCode) {
+    return `userCode:${userCode}`;
+}
+
+const grantable = new Set([
+    'AccessToken',
+    'AuthorizationCode',
+    'RefreshToken',
+    'DeviceCode',
+    'BackchannelAuthenticationRequest',
+]);
+
+class MySuperAdapter {
+    constructor(model) {
+        this.model = model;
+    }
+
+    key(id) {
+        return `${this.model}:${id}`;
+    }
+
+    async destroy(id) {
+        const key = this.key(id);
+        storage.delete(key);
+    }
+
+    async consume(id) {
+        storage.get(this.key(id)).consumed = epochTime();
+    }
+
+    async find(id) {
+        console.log(this.key(id));
+        if (this.model === 'Client') {
+            // My own logic
+            console.log('MySuperAdapter.find(id): ' + id);
+            let redir_uri = TmpClientIdMapRedirUriMap['Client:' + id];
+            console.log(`redir_uri = ${redir_uri}`);
+            return {
+                client_id: id,
+                client_name: 'PublicClient',
+                redirect_uris: [redir_uri],
+                response_types: ['id_token', 'code'],
+                grant_types: ['authorization_code', 'implicit'],
+                token_endpoint_auth_method: 'none', // public client
+            };
+        };
+        return storage.get(this.key(id));
+    }
+
+    async findByUid(uid) {
+        const id = storage.get(sessionUidKeyFor(uid));
+        return this.find(id);
+    }
+
+    async findByUserCode(userCode) {
+        const id = storage.get(userCodeKeyFor(userCode));
+        return this.find(id);
+    }
+
+    async upsert(id, payload, expiresIn) {
+        const key = this.key(id);
+
+        if (this.model === 'Session') {
+            storage.set(sessionUidKeyFor(payload.uid), id, expiresIn * 1000);
+        }
+
+        const { grantId, userCode } = payload;
+        if (grantable.has(this.model) && grantId) {
+            const grantKey = grantKeyFor(grantId);
+            const grant = storage.get(grantKey);
+            if (!grant) {
+                storage.set(grantKey, [key]);
+            } else {
+                grant.push(key);
+            }
+        }
+
+        if (userCode) {
+            storage.set(userCodeKeyFor(userCode), id, expiresIn * 1000);
+        }
+
+        storage.set(key, payload, expiresIn * 1000);
+    }
+
+    async revokeByGrantId(grantId) { // eslint-disable-line class-methods-use-this
+        const grantKey = grantKeyFor(grantId);
+        const grant = storage.get(grantKey);
+        if (grant) {
+            grant.forEach((token) => storage.delete(token));
+            storage.delete(grantKey);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Example config copied from lib
-// const oidc = new Provider(AuthMatterRuntimeConfigManager.get_startup_config().site_hostname, {
-//     clients,
-//     features: {
-//         // pkce: { required: true }, // Enforce PKCE for all
-//         devInteractions: { enabled: true }, // or false if you provide your own UI
-//         clientCredentials: { enabled: false },
-//         introspection: { enabled: false },
-//         revocation: { enabled: false },
-//     },
-//     // Optional: customize how clients are found
-//     clients: [], // empty list for now
-//     findClient: async function (clientId) {
-//         // Return a simple public client object
-//         return {
-//             client_id: clientId,
-//             redirect_uris: [`${AuthMatterRuntimeConfigManager.get_startup_config().site_hostname}/callback/${clientId}`], // you can generalize or load from DB
-//             response_types: ['code'],
-//             grant_types: ['authorization_code'],
-//             token_endpoint_auth_method: 'none', // public client
-//         };
-//     },
-// });
-// oidc.listen(process.env.PORT || 8080);
+const oidc = new Provider(get_config().site_hostname + '/oidc', {
+    async findAccount(ctx, sub, token) {
+        return {
+            accountId: sub,
+            async claims(use, scope, claims, rejected) {
+                return { 'sub': sub };
+            },
+        };
+    },
+    clientDefaults: {
+        response_types: ['code id_token'],
+        grant_types: ['authorization_code', 'implicit'],
+    },
+    claims: {
+        openid: [ 'sub' ],
+        email: [ 'email', 'email_verified' ],
+        profile: [ 'name', 'preferred_username' ],
+    },
+    proxy: true,
+    pkce: { required: false }, // Enforce PKCE for all?
+    features: {
+        devInteractions: { enabled: true }, // or false if you provide your own UI
+        clientCredentials: { enabled: false },
+        introspection: { enabled: false },
+        revocation: { enabled: false },
+    },
+    clients: [],
+    adapter: MySuperAdapter,
+});
+
+function handleClientAuthErrors(ctx, error) {
+    console.log(error);
+    console.log(ctx.request);
+    console.log(ctx.request.header);
+}
+// oidc.on("authorization.accepted", handleClientAuthErrors);
+oidc.on("authorization.error", handleClientAuthErrors);
+
+
+
+
+
+
+
+// Custom middleware to intercept auth requests
+express_gateway.use('/oidc/auth', (req, res, next) => {
+    console.log(`\n\n=====================`);
+    console.log(`express_gateway.use('/oidc/auth'  ...`);
+    console.log(`req.url = ${req.url}`);
+    if (req.query.client_id && req.query.redirect_uri) {
+        // console.log('Intercepted auth request:', req.query);
+        console.log('req.query.client_id:', req.query.client_id);
+        console.log('req.query.redirect_uri:', req.query.redirect_uri);
+        
+        // On-demand ephemeral registration?
+        TmpClientIdMapRedirUriMap['Client:' + req.query.client_id] = req.query.redirect_uri;
+    }
+    // Continue to OIDC provider
+    next();
+});
+
+
+
+
+
+
+
+
 
 
 
@@ -196,7 +384,7 @@ const BusinessLogicHelpers = {
         };
         let user_obj = DatabaseService.get_user_by_uid(uid);
         if (
-            user_obj.org_domain !== AuthMatterRuntimeConfigManager.get_startup_config().org_domain && // Not domestic
+            user_obj.org_domain !== get_config().org_domain && // Not domestic
             DatabaseService.get_roaming_peers_domain_whitelist().indexOf(user_obj) < 0 // Not in whitelist
         ) {
             // Possibly stale user from removed peer?
@@ -224,12 +412,94 @@ const AcceptIncomingRequest = function (cmd, argv, safe_env, req, res) {
 
 
 
+
 // =================================================
 // Express.js gateway setup
 // =================================================
-let express_gateway = express();
+const TmpInteractionMap = {};
+const TmpLoginResultMap = {};
+async function get_login_result(interaction) {
+    // TODO: Really authenticate input credentials with database
+    const accountId = 'neruthes@nekostein.com';
+    // Working with grant?
+    const Grant = oidc.Grant;
+
+    const grant = new Grant({
+        accountId,
+        clientId: interaction.params.client_id,
+    });
+    console.log(`[grant]`);
+    console.log(grant);
+
+    grant.addOIDCScope('openid email profile');
+    grant.addOIDCClaims(['email', 'email_verified']);
+    grant.addResourceScope('urn:example:resource-server', 'read write');
+    const grantId = await grant.save();
+    console.log(`[grantId]`);
+    console.log(grantId);
+
+    let result = {
+        login: {
+            accountId, // logged-in account id
+            acr: '', // acr value for the authentication
+            amr: [], // amr values for the authentication
+            remember: true, // true if authorization server should use a persistent cookie rather than a session one, defaults to true
+        },
+        consent: {
+            grantId, // the identifer of Grant object you saved during the interaction, resolved by Grant.prototype.save()
+        },
+    };
+    return result;
+}
+express_gateway.post("/oidc/interaction/:uid", async (req, res) => {
+    console.log(`>>>    uid is ${req.params.uid}`);
+    // I should verify credentials here...
+    let interaction = await oidc.interactionDetails(req, res);
+    console.log(`[interactionDetails]`);
+    console.log(interaction);
+    console.log('TmpInteractionMap[interaction.jti] = interaction;')
+    TmpInteractionMap[interaction.jti] = interaction;
+    let result = await get_login_result(interaction);
+    console.log(`[result]`);
+    console.log(result);
+    TmpLoginResultMap[interaction.jti] = result;
+
+    // Use oidc-provider builtin redirection
+    // await oidc.interactionFinished(req, res, result);
+
+    // If credential is valid, the user will be redirected to consent page
+    let redirectTo = await oidc.interactionResult(req, res, result);
+    if (!false) { // Config?
+        redirectTo = redirectTo.replace(/^http/, 'https');
+    };
+
+    
+    console.log(`[redirectTo]`);
+    console.log(redirectTo);
+    res.writeHead(302, {
+        Location: redirectTo,
+    });
+    // res.send({ redirectTo });
+    res.end(`Redirecting to  ${redirectTo}  `);
+    console.log(`Ending...`);
+    console.log(`===================================================`);
+});
+
+express_gateway.use('/oidc', oidc.callback());
+
+
+
+
+
+
+
+
+
+
+
+
+// Real user commands
 express_gateway.post('/api/webcmd', function (req, res) {
-    // console.log('11111');
     let req_post_body = '';
     req.on('data', function (req_new_data) { req_post_body += req_new_data });
     req.on('end', function () {
@@ -256,6 +526,18 @@ express_gateway.post('/api/webcmd', function (req, res) {
     });
 });
 
+
+// express_gateway.get('/oidc/.well-known/openid-configuration', function (req, res) {
+//     console.log(`entry  =  ${req.params.entry}`);
+//     end_res_with_json(res, get_config().startup_config_path);
+// });
+
+
+
+
+
+
+// Start HTTP server!
 let port = process.env.PORT || 18800;
 express_gateway.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
